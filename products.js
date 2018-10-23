af6378d796d43524051d1e90654d6120d193cd4a
Celia Marshall <celia.m@gmail.com>
2018-10-23 15:27:33 -0700

    added categories in sidebar

--- products.js
+++ products.js
@@ -8,6 +8,8 @@ function init(){
     const products = document.querySelector('.productListings');
     cardGenerator.render(products, inventory, inventory.length);
 
+    const categories = document.querySelectorAll('.category')
+
 }
 
 

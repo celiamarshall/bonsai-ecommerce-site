541eef20b66e052cea17f21ee729a976712b5ae1
Celia Marshall <celia.m@gmail.com>
2018-10-23 14:25:45 -0700

    adding data

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const inventory = [
       stars: 3
     },
     {
-      title: 'Trailing Cedar',
+      title: 'Trailing Cedar Bonsai',
       img: 'https://secure.img1-fg.wfcdn.com/im/78306317/resize-h800-w800%5Ecompr-r85/4173/41735228/Trailing+Cedar+Artificial+Bonsai+Desk+Top+Plant+in+Pot.jpg',
       description: 'This gracefully arching simulated cedar tree swoops down into a miniature landscape of moss grounded in a sturdy conical pot.',
       price: 39.99,
@@ -16,8 +16,8 @@ const inventory = [
       stars: 4
     },
     {
-      title: 'Suite Bleue',
-      img: 'https://media.architecturaldigest.com/photos/55e76412cd709ad62e8e6854/master/w_640,c_limit/dam-images-architecture-2013-07-tree-houses-treehouse-architecture-01-la-piantata-suite-bleue-italy.jpg',
+      title: 'Juniper Bonsai',
+      img: './bonsai-1.jpg',
       description: 'A centuries-old majestic oak is home to Suite Bleue, one of two tree houses at La Piantata Bed & Breakfast in Italy’s northern Lazio region. The simple red-cedar building was constructed in France by the Provence-based specialty firm La Cabane Perchée and transported to a sprawling field of lavender on the resort’s estate.',
       price: 19.99,
       category: 'Flowering',
@@ -25,7 +25,7 @@ const inventory = [
     },
     {
       title: 'Glass Treehouse',
-      img: 'https://static.dezeen.com/uploads/2017/04/paarman-tree-house-by-mv-architecture-residential_dezeen_hero-1-852x479.jpg',
+      img: './bonsai-2.png',
       description: 'Beautiful glass treehouse',
       price: 25.99,
       category: 'Flowering',
